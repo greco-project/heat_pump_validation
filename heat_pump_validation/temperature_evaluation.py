6b15994b9029fb422308d5b24c94db526088b888
SabineH <sabine.haas@rl-institut.de>
2019-12-17 11:27:45 +0100

    change folder

--- heat_pump_validation/temperature_evaluation.py
+++ heat_pump_validation/temperature_evaluation.py
@@ -8,7 +8,13 @@ import validation_data_upm
 
 path = os.path.join(
         os.path.expanduser('~'),
-        'rl-institut/04_Projekte/220_GRECO/03-Projektinhalte/AP4_High_Penetration_of_Photovoltaics/T4_4_PV_heat_pumps/Data/UPM')
+        'rl-institut/04_Projekte/220_GRECO/03-Projektinhalte/AP4_High_Penetration_of_Photovoltaics/T4_4_PV_heat_pumps/Data/UPM/csv')
+
+# for file in
+#     filename
+#     for filename in os.listdir(
+#             os.path.join(sys.path[0], file_dir))
+
 data = validation_data_upm.read_heat_pump_data(path=path, date='20190711',
                                                mode='COOLING')
 temperature_df = data[['T_ext_IN ', 'T_ext_OUT ', 'T_int_OUT ', 'Tint_IN']]
@@ -18,3 +24,5 @@ hourly_df.plot()
 plt.ylabel('Temperatur in °C')
 plt.grid()
 plt.savefig('temperature_evaluation.pdf')
+
+
