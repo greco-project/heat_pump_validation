aaa4049a4516e697f2fa2e59d29dcbd61587cf28
Stefanie08 <stefanie.nguyen@rl-institut.de>
2020-03-03 12:41:47 +0100

    Add commentary

--- heat_pump_validation/Chillers_validation/Temphub.py
+++ heat_pump_validation/Chillers_validation/Temphub.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
 
-''''
-In this .py EER/COP were calculated first. After that the time was resampled.
-EER_03: Calculated EER with quality grade 0,3, T_ext_in and T_int_OUT
-'''
 
-
-# calc temphub
 def temphub(t_high_series, t_low_series):
+    r"""
+    Calculates the temphub with t_high as heat source and t_low as Wärmesenke(?)
+    Parameters
+    ----------
+
+    :param t_high_series: pd.Series
+    :param t_low_series: pd.Series
+
+    :returns
+    --------
+
+    pd.Series
+    """
     t_high_list = t_high_series.values.tolist()
     t_low_list = t_low_series.values.tolist()
 
@@ -17,8 +24,31 @@ def temphub(t_high_series, t_low_series):
 
     return temphub_series
 
+r"""
+read data and reformat time to datetime
 
-EER_data = pd.read_csv(r'file:///C:\git\data\PV_Chiller_COOLING\20190712\calc_EER_Tint_IN\20190712_calc_EER.csv')  # alle berechneten EER 03-05
+Parameters
+----------
+EER_data: pd.DataFrame
+calculated EER
+
+time_data: pd.DataFrame
+read time from Datalogger data
+
+validation_data: pd.DataFrame
+read measured EER from Tempcontrol data
+
+:returns
+--------
+
+data_nan: pd.DataFrame
+drop all rows filled with NaNs 
+
+data_resampled: pd.Dataframe
+resample of data_nan for every hour  
+
+"""
+EER_data = pd.read_csv(r'file:///C:\git\data\PV_Chiller_COOLING\20190712\calc_EER_Tint_IN\20190712_calc_EER.csv')
 time_data = pd.read_csv(r'file:///C:\git\data\20190712_TempControl_COOLING_SP18_Datalogger.csv')
 validation_data = pd.read_csv(r'file:///C:\git\data\20190712_TempControl_COOLING_SP18_TempControl.csv',
                               encoding='unicode_escape', low_memory=False)
@@ -26,15 +56,14 @@ validation_data = pd.read_csv(r'file:///C:\git\data\20190712_TempControl_COOLING
 time_data['Time'] = time_data['Time'].apply(lambda x: ':'.join(x.split(':')[0:-1]))
 time_data['Time'] = pd.to_datetime(time_data['Time'])
 
-temphub = temphub(t_high_series=time_data['T_ext_IN'], t_low_series=time_data['T_int_IN'])  # calc temphub
+temphub = temphub(t_high_series=time_data['T_ext_IN'], t_low_series=time_data['T_int_IN'])
 validation_series = validation_data['EER']
 
-# resample every 30 minutes / 1 hour and concatenated with temphub and
 data = pd.concat([time_data['Time'], EER_data, temphub, validation_series], axis=1,
                  names=['Time', 'EER']).set_index('Time')
 data_nan = data.replace(0, np.nan).dropna()
 data_resampled = data_nan.resample('H').mean()
-data_resampled.to_csv(r'\\SRV02\RL-Institut\04_Projekte\220_GRECO\03-Projektinhalte\AP4_High_Penetration_of_Photovoltaics\T4_4_PV_heat_pumps\Validierung\PV_Chiller_COOLING\12_11_072019\Tint_IN_Temphub_Tint_IN\Revised\20190712resampled.csv')
-data_nan.to_csv(r'\\SRV02\RL-Institut\04_Projekte\220_GRECO\03-Projektinhalte\AP4_High_Penetration_of_Photovoltaics\T4_4_PV_heat_pumps\Validierung\PV_Chiller_COOLING\12_11_072019\Tint_IN_Temphub_Tint_IN\Revised\20190712all_data.csv')
+data_resampled.to_csv(r'')
+data_nan.to_csv(r'')
 
 
