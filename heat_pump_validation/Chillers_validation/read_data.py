810c5efc7c37b70d238ec2645f07eb721ddc1364
Stefanie08 <stefanie.nguyen@rl-institut.de>
2020-03-03 18:57:17 +0100

    code future settings

---
+++ heat_pump_validation/Chillers_validation/read_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+# COP berechnen
+'''Dann existiert eine csv Datei mit den einzelnen COP-Werten
+diese kann hier evtl eingelesen werden und zusammengetragen werden zu einer großen Datei
+
+'''
+print(os.name)
+print(os.getcwd())
+print(os.path.abspath('.'))
+print(os.listdir('.'))
