5652b3c4bcf851b0a5d7565cc566e22484d3a2a7
Stefanie08 <stefanie.nguyen@rl-institut.de>
2020-04-09 14:41:03 +0200

    Change of parameter for Chiller

--- heat_pump_validation/Chiller_validation/calc_eer.py
+++ heat_pump_validation/Chiller_validation/calc_eer.py
@@ -12,23 +12,12 @@ import oemof.thermal.compression_heatpumps_and_chillers as cmpr_hp_chiller
 import pandas as pd
 
 
-datalogger_1= pd.read_csv(r'file:///C:\git\data\PV_HeatPump_HEATING\20190304\20190304_temp_diff_all.csv',
-                          parse_dates=True, index_col=0)
-data_resample_1 = datalogger_1.resample('H').mean()
-datalogger_2 = pd.read_csv(r'file:///C:\git\data\PV_HeatPump_HEATING\20190301\20190301_temp_diff_all.csv',
-                           parse_dates=True, index_col=0)
-data_resample_2 = datalogger_2.resample('H').mean()
+datalogger_resampled= pd.read_csv(r'file:///C:\git\data\PV_Chiller_COOLING\COOLING_temp_re.csv')
 
-datalogger = pd.concat([datalogger_1, datalogger_2])
-datalogger_resample = pd.concat([data_resample_1, data_resample_2])
-# datalogger.to_csv(r'C:\git\data\PV_HeatPump_HEATING\01_04_032019\data_original.csv')
-# datalogger_resample.to_csv(r'C:\git\data\PV_HeatPump_HEATING\01_04_032019\resampled\data_resampled.csv')
 
-temp_high = datalogger_resample['T_air']
-temp_low = datalogger_resample['T_ext ']
+temp_high = datalogger_resampled['T_ext_IN ']
+temp_low = datalogger_resampled['T_int_OUT ']
 
-#data_t_high_list = data_t_high.values.tolist()
-#data_t_low_list = data_t_low.values.tolist()
 
 cops_chiller = cmpr_hp_chiller.calc_cops(temp_high= temp_high,
                                          temp_low= temp_low,
@@ -38,7 +27,7 @@ cops_chiller = cmpr_hp_chiller.calc_cops(temp_high= temp_high,
 dataseries = pd.DataFrame(cops_chiller)
 
 
-dataseries.to_csv(r'C:\git\data\PV_HeatPump_HEATING\01_04_032019\resampled\COP_resampled\COP_4.csv', decimal='.')
+dataseries.to_csv(r'C:\git\data\PV_Chiller_COOLING\COP\EER_4.csv', decimal='.')
 
 # print("")
 # print("Coefficients of Performance (COP): ", *cops_chiller, sep='\n')
