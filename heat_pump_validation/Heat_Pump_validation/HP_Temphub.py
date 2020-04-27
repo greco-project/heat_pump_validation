2595e571cfe2a654195707b132b6dbcbf88e7588
Stefanie08 <44086480+Stefanie08@users.noreply.github.com>
2020-04-27 09:57:05 +0200

    Update Temphub.py

--- heat_pump_validation/Heat_Pump_validation/HP_Temphub.py
+++ heat_pump_validation/Heat_Pump_validation/HP_Temphub.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def temphub(t_high_series, t_low_series):
     r"""
-    Calculates the temphub with t_high as heat source and t_low as Wärmesenke(?)
+    Calculates the temphub with t_high as heat source and t_low as heat sink
     Parameters
     ----------
 
@@ -36,22 +36,15 @@ validation_data['Time'] = pd.to_datetime(validation_data['Time'])
 validation_data_resampled['Time'] = validation_data_resampled['Time'].apply(lambda x: ':'.join(x.split(':')[0:-1]))
 validation_data_resampled['Time'] = pd.to_datetime(validation_data_resampled['Time'])
 
-# temphub = temphub(t_high_series=validation_data['T_int'],
-#                   t_low_series=validation_data['T_ext '])
 
 temphub_resampled= temphub(t_high_series=validation_data_resampled['T_int'],
                            t_low_series=validation_data_resampled['T_ext '])
 
-validation_series = validation_data['COP']
-
 validation_series_resampled = validation_data_resampled['COP']
 
-# final_data = pd.concat([datalogger, temphub, validation_series], axis=1,
-#                  names=['Time', 'COP']).set_index('Time')
 
 final_data_resampled = pd.concat([datalogger_resampled,
                                   temphub_resampled, validation_series_resampled], axis=1,
                                  names=['Time', 'COP']).set_index('Time')
 
 final_data_resampled.to_csv(r'C:\git\data\PV_HeatPump_HEATING\01_04_032019\resampled\final_data_resampled.csv')
-#final_data.to_csv(r'C:\git\data\PV_HeatPump_HEATING\01_04_032019\final_data.csv')
