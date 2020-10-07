c5ab528828aa5f7cd6e6b62945046087b76882ea
Marie Gering <marie-claire.gering@rl-institut.de>
2020-10-07 09:58:14 +0200

    Improve read data in calc_eer.py and fix names of params

--- heat_pump_validation/Chiller_validation/calc_eer.py
+++ heat_pump_validation/Chiller_validation/calc_eer.py
@@ -17,16 +17,66 @@ path_preprocessed_data = os.path.abspath(os.path.join(path_file, os.pardir, os.p
                                                       'results', 'chiller'))
 
 # Read data original and resampled from 11.07.2019 and 12.07.2019
+try:
+    datalogger_1 = pd.read_csv(os.path.join(path_preprocessed_data, 'original', '20190711_COOLING_temp.csv'),
+                               parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_SP18.py first.\n')
+try:
+    datalogger_resampled_1 = pd.read_csv(os.path.join(path_preprocessed_data, 'resampled',
+                                                      '20190711_COOLING_temp_re.csv'), parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_SP18.py first.\n')
 
+try:
+    datalogger_2 = pd.read_csv(os.path.join(path_preprocessed_data, 'original', '20190712_COOLING_temp.csv'),
+                           parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_SP18.py first.\n')
 
-temp_high = datalogger_resampled['T_ext_IN ']
-temp_low = datalogger_resampled['T_int_OUT ']
+try:
+    datalogger_resampled_2 = pd.read_csv(os.path.join(path_preprocessed_data, 'resampled',
+                                                      '20190712_COOLING_temp_re.csv'), parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_SP18.py first.\n')
 
+try:
+    datalogger_3 = pd.read_csv(os.path.join(path_preprocessed_data, 'original', '20190115_COOLING_temp.csv'),
+                               parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_MPPT.py first.\n')
 
-cops_chiller = cmpr_hp_chiller.calc_cops(temp_high= temp_high,
-                                         temp_low= temp_low,
-                                         quality_grade=0.4,
-                                         mode='heat_pump')
+try:
+    datalogger_resampled_3 = pd.read_csv(os.path.join(path_preprocessed_data, 'resampled',
+                                                      '20190115_COOLING_temp_re.csv'), parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_MPPT.py first.\n')
+
+try:
+    datalogger_4 = pd.read_csv(os.path.join(path_preprocessed_data, 'original', '20190120_COOLING_temp.csv'),
+                               parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_MPPT.py first.\n')
+
+try:
+    datalogger_resampled_4 = pd.read_csv(os.path.join(path_preprocessed_data, 'resampled',
+                                                      '20190120_COOLING_temp_re.csv'), parse_dates=True, index_col=0)
+except FileNotFoundError:
+    print('\nData could not be read. It may not exist yet. Please run Chiller_pre_processing_MPPT.py first.\n')
+
+# Concatenate data of the two dates for original and sampled data
+datalogger = pd.concat([datalogger_1, datalogger_2, datalogger_3, datalogger_4])
+datalogger_resampled = pd.concat([datalogger_resampled_1, datalogger_resampled_2,
+                                  datalogger_resampled_3, datalogger_resampled_4])
+
+# Save
+datalogger.to_csv(os.path.join(path_preprocessed_data, 'original', 'data_original.csv'))
+datalogger_resampled.to_csv(os.path.join(path_preprocessed_data, 'resampled', 'data_resampled.csv'))
+
+temp_high = datalogger['T_ext']
+temp_high_resampled = datalogger_resampled['T_ext']
+temp_low = datalogger['T_air']
+temp_low_resampled = datalogger_resampled['T_air']
 
 dataseries = pd.DataFrame(cops_chiller)
 
